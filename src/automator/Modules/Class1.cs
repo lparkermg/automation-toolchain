1a219d8066f9487622a1fd39096f78cf7f897f30
Luke Parker <moongateuk@gmail.com>
2017-10-12 10:10:37 +0100

    Added main source.

---
+++ src/automator/Modules/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Modules.Interfaces;
+
+namespace Modules
+{
+    public class GameDevSetupModule : IAutoModule
+    {
+        public bool Run<T>(T[] data)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/automator/Modules/Interfaces/IAutoModule.cs
@@ -0,0 +1,7 @@
+﻿namespace Modules.Interfaces
+{
+    public interface IAutoModule
+    {
+        bool Run<T>(T[] data);
+    }
+}
